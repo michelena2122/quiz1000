fc970d1f9653aae375024dd41617e942a7934ff0
Juan Michelena <juanmichelena@Juans-MacBook-Air.local>
2026-04-27 20:10:32 -0600

    server: eliminar require duplicado de express-rate-limit

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const rateLimit = require("express-rate-limit");
 const nodemailer = require("nodemailer");
-const rateLimit = require("express-rate-limit");
 const cors = require("cors");
 const path = require("path");
 const fs = require("fs");
